351d12e0f333d1ba999dfa4e2ec7ed470bfc0e79
Ivan Altamirano <ivan.altamirano@gmail.com>
2013-02-08 19:13:45 -0700

    connection to production environment

--- config/connection.php
+++ config/connection.php
@@ -1,7 +1,7 @@
 <?php
 
 //Development
-
+/*
 function getConnection() {
     $dbhost="localhost";
     $dbuser="root";
@@ -24,13 +24,14 @@ ActiveRecord\Config::initialize(function($cfg){
     //$cfg->set_default_connection('development');
 
 });
+*/
 
 //Production
-/*
+
 function getConnection() {
     $dbhost="tunnel.pagodabox.com";
-    $dbuser="analisa";
-    $dbpass="gPBehkCR";
+    $dbuser="dina";
+    $dbpass="HdLBrlYX";
     $dbname="tavenposdb";
     $dbh = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -38,7 +39,16 @@ function getConnection() {
 }
 
 ORM::configure('mysql:host=tunnel.pagodabox.com;dbname=tavenposdb');
-ORM::configure('username', 'analisa');
-ORM::configure('password', 'gPBehkCR');
-*/
+ORM::configure('username', 'dina');
+ORM::configure('password', 'HdLBrlYX');
+
+ActiveRecord\Config::initialize(function($cfg){
+
+    $cfg->set_model_directory('models');
+    $cfg->set_connections(array('development' => 'mysql://root:dina@tunnel.pagodabox.com/tavenposdb'));
+    //$cfg->set_default_connection('development');
+
+});
+
+
 ?>
